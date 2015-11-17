7130f4009ce13c07560a83de41538b0b0282d65d
OneB1t <onebit@seznam.cz>
2015-11-18 00:51:07 +0100

    Update omni_alto5.mk

--- omni_alto5.mk
+++ omni_alto5.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/alcatel/alto45/full_alto5.mk)
+$(call inherit-product, device/alcatel/alto5/full_alto5.mk)
 
 PRODUCT_NAME := omni_alto5
